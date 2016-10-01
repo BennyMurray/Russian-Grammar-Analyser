bfd8a816037bd3ff3fe2084d1c39efcf21640355
Benjamin Murray <bjamurray@gmail.com>
2016-10-01 14:37:49 +0100

    Refined Site
    Tidied up site. Added comments.

--- __init__.py
+++ __init__.py
@@ -7,14 +7,11 @@ from flask import Flask, jsonify,request,render_template
 from translator import analyseGrammar
 sys.setdefaultencoding('utf-8')
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('computer.html')
-
-
 
 @app.route('/_add_numbers')
 def add_numbers():
@@ -22,12 +19,7 @@ def add_numbers():
 	list = []
 	dict = analyseGrammar(user_input.lstrip(' '))
 	list.append(dict)
-	#b = getTranslation(a)
 	return jsonify(result=list)
-
-
-	
-	
 
 if __name__ == '__main__':
     app.run(debug=True)
